933c3aac65c630e6a8483567b0cbcb7d6bff854f
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2022-02-25 11:09:53 +0200

    hello

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import utils.DatabaseConnection;
 public class Main {
 
     public static void main(String[] args) {
+
         DatabaseConnection dbConn = new DatabaseConnection();
+        System.out.println("Hello world!!!");
     }
 }

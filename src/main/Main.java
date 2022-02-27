55d5ff81c0c7696e0726f2ed7b68d94e187292e5
Mmanzi monnierey <mudahemukamanzi@yahoo.com>
2022-02-27 10:38:47 +0200

    returned dbconnection

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,11 +2,16 @@ package main;
 
 import utils.DatabaseConnection;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         DatabaseConnection dbConn = new DatabaseConnection();
+
         System.out.println("Hello world!!!");
     }
 }

7bd2f58034e6e3a01421c4c9fcd6511443a92cf8
IsiteYves <yvesisite@gmail.com>
2022-02-28 10:21:55 +0200

    Merge branch 'user_management' of https://github.com/class-2-b-2022/logistics-server into user_management

--- src/services/UserService.java
+++ src/services/UserService.java
@@ -14,49 +14,63 @@ import java.sql.SQLException;
 public class UserService {
 	static Utils.DatabaseConnection conn = new Utils.DatabaseConnection();
 
-	public static void insertUser(User user) throws SQLException {
-		String sql = "INSERT INTO user(names,email,phone,password,role)values(?,?,?,?,?)";
-		PreparedStatement stmt = conn.getConnection().prepareStatement(sql);
-		stmt.setString(1, user.getNames());
-		stmt.setString(2, user.getEmail());
-		stmt.setInt(3, user.getPhone());
-		stmt.setString(4, user.getPassword());
-		stmt.setString(5, user.getRole());
-		stmt.executeUpdate();
+	public void insertUser(User user) throws SQLException {
+		boolean userCheck = checkIfUserExists(user.getEmail());
+		if(!userCheck){
+			String sql = "INSERT INTO user(names,email,phone,password,role)values(?,?,?,?,?)";
+			PreparedStatement stmt = conn.getConnection().prepareStatement(sql);
+			stmt.setString(1, user.getNames());
+			stmt.setString(2, user.getEmail());
+			stmt.setInt(3, user.getPhone());
+			stmt.setString(4, user.getPassword());
+			stmt.setString(5, user.getRole());
+			int Created=stmt.executeUpdate();
+			if(Created==1){
+				System.out.println("User Created Successfully");
+			}else{
+System.out.println("Error creating the user.");
+			}
+		}else{
+			System.out.println("User already Exists");
+		}
 	}
 
-	public static String findUser(User user) throws SQLException {
-		int userCheck = (checkIfUserExists(user.getEmail(), user.getPassword()));
-		if (userCheck != -1)
+	public String findUser(User user) throws SQLException {
+		boolean userCheck = checkIfUserExists(user.getEmail());
+		if (!userCheck)
 			return "User not found";
 		else {
 			String sql2 = "SELECT role FROM roles WHERE role_id=?";
 			PreparedStatement roleIdStmt = conn.getConnection().prepareStatement(sql2);
-			roleIdStmt.setInt(1, userCheck);
+			roleIdStmt.setInt(1, 5);
 			ResultSet rs2 = roleIdStmt.executeQuery();
+			String returnValue="not found";
 			while (rs2.next())
-				return rs2.getString(1);
-			return "not found";
+				returnValue=rs2.getString(1);
+			return returnValue;
 		}
 	}
 
-	public static int checkIfUserExists(String email, String password) throws SQLException {
-		String sql = "SELECT role FROM users WHERE email=? AND password=?";
+	public static boolean checkIfUserExists(String email) throws SQLException {
+		String sql = "SELECT role FROM users WHERE email=?";
 		PreparedStatement selectStmt = conn.getConnection().prepareStatement(sql);
 		selectStmt.setString(1, email);
-		selectStmt.setString(2, password);
 		ResultSet rs = selectStmt.executeQuery();
-		int userCount = 0;
-		while (rs.next()) {
-			userCount++;
+		boolean userExists = false;
+		if (rs.next()) {
+			System.out.println("Found...."+rs.getInt(1));
+			userExists= true;
 		}
-		if (userCount == 0)
-			return -1;
-		else
-			return rs.getInt(1);
+		return userExists;
 	}
 
 	public static void main(String[] args) throws IOException, SQLException {
-		findUser(new User());
+		// Test Login
+		UserService s1=new UserService();
+		User u1=new User();
+		u1.setEmail("yvesisite@gmail.com");
+		u1.setNames("Isite");
+		u1.setPassword("pass123");
+		System.out.println(s1.findUser(u1));
 	}
 }

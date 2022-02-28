74cffd4a5d95bd5da727eda392e3fe3f1ea1c6d6
nellybutera <85675700+nellybutera@users.noreply.github.com>
2022-02-28 05:41:21 +0200

    can select resellers

--- src/Services/Distributors/ResellerService.java
+++ src/Services/Distributors/ResellerService.java
@@ -3,9 +3,9 @@ package services.Distributors;
 import models.Distributors.Reseller;
 import utils.DatabaseConnection;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ResellerService {
     private DatabaseConnection databaseConnection = new DatabaseConnection();
@@ -21,6 +21,28 @@ public class ResellerService {
         preparedStatement.setString(5, reseller.getBusiness_name());
         preparedStatement.setDate(6,reseller.getCreatedAt());
         preparedStatement.executeUpdate();
+    }
+
+    public List<Object> getListOfResellers() throws SQLException {
+        List<Object> resellersObject = new ArrayList();
+        String sql = "SELECT * FROM resellers";
+        Statement statement = connection.createStatement();
+        ResultSet result = statement.executeQuery(sql);
+
+        int count = 0;
+
+        while(result.next()){
+            String first_name = result.getString("first_name");
+            String last_name = result.getString("last_name");
+            Integer telephone = result.getInt("telephone");
+            String email = result.getString("email");
+            String business_name = result.getString("business_name");
+            Date Date = result.getDate("createdAt");
+
+            String output = "User #%d: %s -%s - %s -%s";
+            System.out.println(String.format(output,++count,first_name,last_name,telephone,email,business_name));
+        }
+        return resellersObject;
     }
 
 }

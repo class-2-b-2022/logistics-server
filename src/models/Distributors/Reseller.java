06bead3da8913aac5e8c70f8f7cb2cd73e99be1b
nellybutera <85675700+nellybutera@users.noreply.github.com>
2022-02-28 05:06:03 +0200

    reseller model done

--- src/models/Distributors/Reseller.java
+++ src/models/Distributors/Reseller.java
@@ -23,6 +23,48 @@ public class Reseller {
         this.business_name = business_name;
         this.createdAt = createdAt;
     }
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) { this.id = id;   }
+
+    public String getFirst_name() {   return first_name;   }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name= last_name;
+    }
+
+    public Integer getTelephone() {  return telephone; }
+
+    public void setTelephone(Integer telephone) { this.telephone = telephone;  }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getBusiness_name() {
+        return business_name;
+    }
+
+    public void setBusiness_name(String business_name) { this.business_name = business_name; }
+
+    public Date getCreatedAt() { return createdAt;    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 
 }

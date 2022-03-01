c71810cd1b531014fcc810a6d03f6b5a777c4b63
IsiteYves <yvesisite@gmail.com>
2022-03-01 22:08:44 +0200

    Refactored logistic server

--- src/thread/ClientManager.java
+++ src/thread/ClientManager.java
@@ -1,9 +1,12 @@
 package thread;
 
-import controllers.DeliveryModule.VehicleManagementController;
+import controllers.user_management.UserController;
 import models.BillingModel;
 import models.ClientRequest;
-import java.io.*;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.List;
 
@@ -42,7 +45,7 @@ public class ClientManager implements Runnable{
 //                        logic related to company registration
                         break;
                     case "/users":
-                    	  responseData=UserController.mainMethod(clientRequest);
+                    	  responseData= UserController.mainMethod(clientRequest);
 //                        logic related to user management
                         break;
                     case "/inventory":

bba355b4f87bea9b15493c20abe6a5c8a791c457
nellybutera <85675700+nellybutera@users.noreply.github.com>
2022-04-21 09:59:19 +0200

    added  company service

---
+++ src/services/CompanyService.java
@@ -0,0 +1,6 @@
+public class CompanyService {
+    // connecting to database
+    private DatabaseConnection databaseConnection = new DatabaseConnection();
+    Connection con = databaseConnection.getConnection();
+
+}

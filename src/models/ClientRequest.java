09ada83bec41dbd6137493f40903c463f6220c4d
nellybutera <85675700+nellybutera@users.noreply.github.com>
2022-05-03 10:21:37 +0200

    removed bugs

--- src/models/ClientRequest.java
+++ src/models/ClientRequest.java
@@ -2,16 +2,8 @@ package models;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import java.io.Serializable;
-<<<<<<< HEAD
 import java.util.Iterator;
 import java.util.Map;
-=======
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-
->>>>>>> de9f252aa394f2d7ca7aceb028c578bbd4ab9120
 /**
  * @author : Mudahemuka Manzi
  */
@@ -49,5 +41,4 @@ public class ClientRequest implements Serializable {
     public void setData(Object data) {
         this.data = data;
     }
-
 }
--- src/services/user_services/UserService.java
+++ src/services/user_services/UserService.java
@@ -3,23 +3,21 @@ package services.user_services;
 import models.user_model.User;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import utils.DatabaseConnection;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserService {
     private DatabaseConnection databaseConnection = new DatabaseConnection();
     Connection connection = databaseConnection.getConnection();
-   
+
     private String hashPassword(String password) {
-    	 //       System.out.println(BCrypt.gensalt(12));
-    	        return BCrypt.hashpw(password, BCrypt.gensalt(12));
+        //       System.out.println(BCrypt.gensalt(12));
+        return BCrypt.hashpw(password, BCrypt.gensalt(12));
     }
-                     
+
     public User findUser(User user) throws Exception {
         User returnObject = new User();
         if (isEmailRegistered(user.getEmail()).getNames() == null)
@@ -42,21 +40,19 @@ public class UserService {
 
     public User getUserInfo(String email, String password) throws SQLException {
         User returnObject = new User();
-        String sql = "SELECT user_id,names,phone,role,password FROM users WHERE email=? LIMIT 1";
+        String sql = "SELECT names,phone,password FROM users WHERE email=? LIMIT 1";
         PreparedStatement stmt = connection.prepareStatement(sql);
         stmt.setString(1, email);
         ResultSet rs = stmt.executeQuery();
         String dbPswd;
         while (rs.next()) {
-            dbPswd=rs.getString(5);
+            dbPswd=rs.getString(3);
             returnObject.setRoleAsString(getRoleAsString(email));
             boolean arePswdsTheSame=BCrypt.checkpw(password,dbPswd);
             if(arePswdsTheSame) {
-                returnObject.setUserId(rs.getInt(1));
-                returnObject.setNames(rs.getString(2));
+                returnObject.setNames(rs.getString(1));
                 returnObject.setEmail(email);
-                returnObject.setPhone(rs.getInt(3));
-                returnObject.setRole(rs.getInt(4));
+                returnObject.setPhone(rs.getInt(2));
             }
             break;
         }
@@ -89,29 +85,17 @@ public class UserService {
         return returnObject;
     }
 
-    private boolean isEmailAlreadyTaken(int userId, String email) throws SQLException {
-        String sql = "SELECT * FROM users WHERE userId<>? AND email=? LIMIT 1";
-        PreparedStatement stmt = connection.prepareStatement(sql);
-        stmt.setInt(1, userId);
-        stmt.setString(2, email);
-        ResultSet rs= stmt.executeQuery();
-        boolean isTaken=false;
-        while(rs.next()) {
-            isTaken=true;
-        }
-        return isTaken;
-    }
-
     public User updateUser(User user) throws  SQLException {
-        User returnObject=new User();
-        boolean isEmailTaken = isEmailAlreadyTaken(user.getUserId(),user.getEmail());
-        if(!isEmailTaken) {
-            String sql = "UPDATE users SET email=?,names=?,phone=? WHERE id=?";
+        User returnObject;
+        returnObject = isEmailRegistered(user.getEmail());
+        if(returnObject.getEmail() != null) {
+            String sql = "UPDATE users SET email=?,names=?,phone=?,role=? WHERE id=?";
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, user.getEmail());
             stmt.setString(2, user.getNames());
             stmt.setInt(3, user.getPhone());
-            stmt.setInt(4, user.getUserId());
+            stmt.setInt(4, user.getRole());
+            stmt.setInt(5, user.getUserId());
             stmt.executeUpdate();
             return user;
         }else{
@@ -134,8 +118,8 @@ public class UserService {
     public boolean insertUser(User user) throws Exception {
         boolean userExists = checkIfUserExists(user.getEmail());
         if (!userExists) {
-           String sql = "INSERT INTO users(names,email,phone,password,role)values(?,?,?,?,?)";
-           String hashedPswd = hashPassword(user.getPassword());
+            String sql = "INSERT INTO users(names,email,phone,password,role)values(?,?,?,?,?)";
+            String hashedPswd = hashPassword(user.getPassword());
 //           System.out.println(BCrypt.checkpw(user.getPassword(),"$2a$12$Da8MomfA5DXHcJpGXnX7e.VNne3B7NDHGcJlU3dFL7cMd0efQ1DLC"));
 //           System.out.println(hashedPswd);
             PreparedStatement stmt = connection.prepareStatement(sql);
@@ -158,25 +142,6 @@ public class UserService {
         }
         return false;
     }
-    
-	public List<Object> allUsers() throws SQLException {
-		 List<Object> users = new ArrayList<Object>();
-	        String sql = "SELECT * FROM users";
-	        Statement stmt = connection.createStatement();
-	        ResultSet res = stmt.executeQuery(sql);
-	        while (res.next()){
-	           User user = new User();
-	            user.setUserId(res.getInt("user_id"));
-	            user.setNames(res.getString("names"));
-	            user.setEmail(res.getString("email"));
-	            user.setPhone(res.getInt("phone"));
-	            user.setRoleAsString(getRoleAsString(res.getString("email")));
-	            user.setStatus(res.getString("status"));
-	            users.add((Object) user);
-	        }
-	       
-		return users;
-	}
-    
+
 
 }
